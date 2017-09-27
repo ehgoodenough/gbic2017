4dde3f8b812b0e4295d1cf417c7fa67574205ff7
Andrew McPherson <ehgoodenough@gmail.com>
2017-09-26 23:32:05 -0700

    Added planets in the background.

--- source/scripts/Game.js
+++ source/scripts/Game.js
@@ -26,6 +26,8 @@ export default class Game extends Pixi.Container {
             this.addChild(new Star())
         }
 
+        this.addChild(new Planet())
+
         this.addChild(this.scene = new Scene())
     }
     update(delta) {
@@ -37,5 +39,36 @@ export default class Game extends Pixi.Container {
     }
     render() {
         this.renderer.render(this)
+    }
+}
+
+var TEXTURES = undefined
+
+class Planet extends Pixi.Sprite {
+    constructor() {
+        TEXTURES = TEXTURES || [
+            Pixi.Texture.from(require("images/planet-1.png")),
+            Pixi.Texture.from(require("images/planet-2.png")),
+            Pixi.Texture.from(require("images/planet-3.png")),
+            Pixi.Texture.from(require("images/planet-4.png")),
+            Pixi.Texture.from(require("images/planet-5.png")),
+        ]
+        super(TEXTURES[0])
+        TEXTURES.push(TEXTURES.shift())
+
+        this.position.x = Math.random() * FRAME.WIDTH
+        this.position.y = Math.random() * FRAME.HEIGHT * (1/4)
+
+        this.speed = 1
+    }
+    update(delta) {
+        this.position.x -= this.speed
+        if(this.position.x <= 0 - this.width) {
+            this.position.x += (2 * this.width) + FRAME.WIDTH
+            this.position.y = Math.random() * FRAME.HEIGHT * (1/4 )
+
+            this.texture = TEXTURES[0]
+            TEXTURES.push(TEXTURES.shift())
+        }
     }
 }
