ba14f5fb04bc6c103ba31310fda4d41b97760342
Andrew McPherson <ehgoodenough@gmail.com>
2017-09-26 23:22:24 -0700

    Experimented with some other colors.

--- source/scripts/Constants.js
+++ source/scripts/Constants.js
@@ -7,6 +7,12 @@ export const COLORS = {
     3: 0xFFBD44,
     4: 0xCEBCFF,
     5: 0x00A6FF,
+
+    // 1: 0x0DE5D3,
+    // 2: 0xAAF2AD,
+    // 3: 0xFFDE2E,
+    // 4: 0xEFAE9D,
+    // 5: 0xFFFFFF,
 }
 
 export const TILE = 33 + 2
